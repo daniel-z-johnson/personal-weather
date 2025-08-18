5ac6014492bde87e07a68fd41d36def9b3601323
daniel-z-johnson <danzaljohnson@gmail.com>
2025-08-17 20:31:52 -0500

    style and logging

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+
 	"github.com/daniel-z-johnson/personal-weather/config"
 	"github.com/daniel-z-johnson/personal-weather/controllers"
 	"github.com/daniel-z-johnson/personal-weather/models"
@@ -11,23 +15,21 @@ import (
 	"github.com/go-chi/chi/v5"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pressly/goose/v3"
-	"log/slog"
-	"net/http"
-	"os"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	db, err := sql.Open("sqlite3", "w.db")
 	if err != nil {
 		slog.Error("Failed to open database", slog.Any("error", err))
 		panic(fmt.Errorf("Failed to open database: %w", err))
 	}
 	goose.SetDialect("sqlite3")
+	goose.SetLogger(&SlogGooseLogger{Logger: logger})
 	if err := goose.Up(db, "migrations"); err != nil {
 		slog.Error("Failed to up migrations", slog.Any("error", err))
 		panic(fmt.Errorf("Failed to up migrations: %w", err))
 	}
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	logger.Info("Personal Weather start")
 	conf, err := config.LoadConfig("config.json")
 	if err != nil {
@@ -35,7 +37,6 @@ func main() {
 		panic(err)
 	}
 	logger.Info(fmt.Sprintf(conf.String()))
-
 	weatherAPI := &models.OpenWeatherAPI{Logger: logger, APIKey: conf.WeatherAPI.Key}
 	weatherService := &models.WeatherService{DB: db, Logger: logger}
 	weatherController, err := controllers.NewWeather(logger, weatherAPI, weatherService)
@@ -58,4 +59,21 @@ func main() {
 		logger.Error("Failed to start server", slog.Any("error", err))
 		panic(fmt.Errorf("Failed to start server: %w", err))
 	}
+}
+
+type SlogGooseLogger struct {
+	Logger *slog.Logger
+}
+
+func (l *SlogGooseLogger) Printf(format string, v ...interface{}) {
+	l.Logger.Info("goose", "msg", fmt.Sprintf(format, v...))
+}
+
+func (l *SlogGooseLogger) Fatalf(format string, v ...interface{}) {
+	l.Logger.Error("goose fatal", "msg", fmt.Sprintf(format, v...))
+	os.Exit(1)
+}
+
+func (l *SlogGooseLogger) Error(v ...interface{}) {
+	l.Logger.Error("goose error", "msg", fmt.Sprint(v...))
 }
